2e403c062aeefd87ee086074829da54b20b02498
Liam Greenway <ApplePiePc@outlook.com>
2025-02-26 21:46:29 -0500

    codecrafters submit [skip ci]

--- src/Main.cs
+++ src/Main.cs
@@ -3,8 +3,10 @@ using System.Net.Sockets;
 
 public class XSH {
     public static void Main(string[] args) {
-        Console.Write("$ ");
-        string command = Console.ReadLine();
-        Console.WriteLine($"{command}: command not found");
+        while (true) {
+            Console.Write("$ ");
+            string command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        }
     }
 }
